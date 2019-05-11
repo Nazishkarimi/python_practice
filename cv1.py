fea49643db3a4cd877355e2bbbaa659982c663ed
Nazishkarimi <46483946+Nazishkarimi@users.noreply.github.com>
2019-05-11 13:58:41 +0500

    Add files via upload

---
+++ cv1.py
@@ -0,0 +1,14 @@
+name = 'Nazish Karimi'
+father_name = 'M Hadis Karimi'
+cnic = '42000-000000-0'
+address = 'House # 00, Sector 00 ,Karachi'
+
+
+
+print('Name :'+ name)
+print('Father Name :'+ father_name)
+print('CNIC :'+ cnic)
+print('address :'+ address)
+
+
+
